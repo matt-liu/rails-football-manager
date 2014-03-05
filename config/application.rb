e6807180cf39e009e5de29ab3a2111821bb94066
loos <louisknapp@gmail.com>
2014-03-05 10:39:07 -0600

    test jenkins (again)

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Test jenkins AGAIN
+# Test jenkins Third Time
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
