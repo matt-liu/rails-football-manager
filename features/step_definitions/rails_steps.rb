b17a38c51fa1a0f9eb455d2fac105e8540441f0a
ParkDan <dpinch86@gmail.com>
2014-03-04 18:00:43 -0600

    [3](Dan) fixed broken tests

--- features/step_definitions/rails_steps.rb
+++ features/step_definitions/rails_steps.rb
@@ -1,7 +1,6 @@
 Given(/^I am on the home page$/) do
   visit root_path
 end
- 
 
 When(/^there is a user named Dan$/) do
 	FactoryGirl.create(:player, name: "Dan")
@@ -10,14 +9,13 @@ end
 When(/^I click on the Players button$/) do
 	click_link("players_id")
 end
-  	
+
 Then(/^I should see a list of the players information$/) do
   page.should have_content("Dan")
   page.should have_content("TW")
   page.should have_content(1)
   page.should have_content(25)
 end
-
 
 When(/^I search for a player by their name$/) do
   fill_in('search', :with => 'Dan')
@@ -28,8 +26,6 @@ When(/^I search for a player by their number$/) do
 	fill_in('search', :with => '1')
 	click_button('Search')
 end
-
-
 
 Then(/^I see that player's information displayed$/) do
  	page.should have_content("Dan")
