633f66d328a86e4cffb9dcb85496720930441a78
ParkDan <dpinch86@gmail.com>
2014-03-05 15:01:26 -0600

    merged and minor naming changes

--- spec/views/players/index.html.erb_spec.rb
+++ spec/views/players/index.html.erb_spec.rb
@@ -16,13 +16,22 @@ describe "players/index" do
         :age => 2
       )
     ])
+
+    assign(:coaches, [
+      stub_model(Coach,
+        :name => "Name",
+        :team => "Team",
+        :title => "Head"
+      )
+    ])
   end
 
-  it "renders a list of players" do
+  it "renders a list of players and coaches" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Team".to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 3
+    assert_select "tr>td", :text => "Team".to_s, :count => 3
+    assert_select "tr>td", :text => "Head".to_s, :count => 1
     assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => 2.to_s, :count => 2
   end
