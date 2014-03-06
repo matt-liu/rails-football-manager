ac463fa315b50b7ddc91053dfb6e9898d676d06c
Matthew Liu <mattliu@thoughtworks.com>
2014-03-06 15:10:12 -0600

    Made team names show up instead of IDs

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 describe Team do
-  it 'should not be valid without a name' do
-  	invalid_team = FactoryGirl.build(:team, name: nil)
-  	puts "#{invalid_team.inspect}"
-  	invalid_team.valid?.should be(false)
-  end
+	context 'when creating a team' do
+		it 'should not be valid without a name' do
+			invalid_team = FactoryGirl.build(:team, name: nil)
+	  		puts "#{invalid_team.inspect}"
+	  		invalid_team.valid?.should be(false)
+	  	end
 
-  it 'should be valid with a name' do
-  	valid_team = FactoryGirl.build(:team)
-  	valid_team.valid?.should be(true)
-  end
+		it 'should be valid with a name' do
+		  	valid_team = FactoryGirl.build(:team)
+		  	valid_team.valid?.should be(true)
+		end
+	end
+
+	context 'Team.get_name_by_id' do
+  		it 'should return team name when passed team id' do
+  			
+  		end
+  	end
 end
