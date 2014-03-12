488368f662e9a3ceac4dc911370de92243ec46e7
ParkDan <dpinch86@gmail.com>
2014-03-12 14:36:33 -0500

    changed seeds file queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,15 +20,15 @@ load_team.each { |array|
 	}
 
 load_player.each { |array|
-		Player.create name: array[0], age: array[1], number: array[2], team: Team.where('name IS ?', array[3]).first
+		Player.create name: array[0], age: array[1], number: array[2], team: Team.where(name: array[3]).first
 	}
 
 load_game.each { |array|
-		Game.create home_team: Team.where('name IS ?', array[0]).first, away_team: Team.where('name IS ?', array[1]).first, home_team_score: array[2], away_team_score: array[3], date: array[4]
+		Game.create home_team: Team.where(name: array[0]).first, away_team: Team.where(name: array[1]).first, home_team_score: array[2], away_team_score: array[3], date: array[4]
 	}
 
 load_coach.each { |array|
-		Coach.create name: array[0], title: array[1], team: Team.where('name IS ?', array[2]).first
+		Coach.create name: array[0], title: array[1], team: Team.where(name: array[2]).first
 	}
 
 
