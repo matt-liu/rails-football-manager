d29e78fc4ad00cc7e6e2f8f544369fca87250df3
ParkDan <dpinch86@gmail.com>
2014-03-13 11:20:03 -0500

    updated validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,9 @@
 class Game < ActiveRecord::Base
 	belongs_to :home_team, class_name: 'Team' , foreign_key: 'home_team_id'
 	belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
-	validates :home_team_score, :away_team_score, :home_team_id, :date, :away_team_id, presence: true
+	validates :home_team_id, :date, :away_team_id, presence: true
 	validate :home_and_away_teams_are_different
-	validates :home_team_score, :numericality => { :greater_than_or_equal_to => 0 }
-	validates :away_team_score, :numericality => { :greater_than_or_equal_to => 0 }
+	validates :home_team_score, :away_team_score, :numericality => { :greater_than_or_equal_to => 0 }
 
 	def home_and_away_teams_are_different
 		if home_team_id == away_team_id
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,12 @@
 class Player < ActiveRecord::Base
 	belongs_to :team
 	validates :name, :number, :age, presence: true
-	
+
   def self.find_player(search_params)
-    Player.where("name LIKE ? OR number LIKE ?", "%#{search_params}%", "%#{search_params}%")
+    # Player.where("name LIKE ? OR number LIKE ?", "%#{search_params}%", "%#{search_params}%")
+    Player.where(name: search_params).to_a.concat(Player.where(number: search_params).to_a)
+    # Player.where("name LIKE #{search_params}").to_a.concat(Player.where("number LIKE #{search_params}").to_a)
+    # Player.find(:all, :conditions => ["name like :search || number like :search", {search: "%#{search_params}%"}])
   end
+
 end
