eee31e14117baf3d5377c5d149c4f07c023803ec
Steve Winter <spwntr@gmail.com>
2014-03-13 18:11:41 -0500

    postgres player search

--- app/models/player.rb
+++ app/models/player.rb
@@ -11,7 +11,7 @@ class Player < ActiveRecord::Base
     # Player.where("name = #{search_params}").to_a.concat(Player.where("number = #{search_params}").to_a)
     # Player.where(:name.to_s.include? search_params).to_a.concat(Player.where(:number.to_s.include? search_params).to_a)
   	# Player.where("name ~ search_params")
-    Player.where("name like ?", "#{search_params}").to_a.concat(Player.where(number: search_params).to_a)
+    Player.where("name ~ ?", "#{search_params}").to_a.concat(Player.where(number: search_params).to_a)
     # Player.where(name: search_params).to_a.concat(Player.where(number: search_params).to_a)
     # Player.where(:name.to_s.include? search_params).to_a
     # Player.find(:all, :conditions => ["name like :search || number like :search", {search: "%#{search_params}%"}])
